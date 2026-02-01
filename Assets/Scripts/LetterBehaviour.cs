f0265a8188204737088041e92d1f9e194ba929f0
TaylorJCassidy <taylorjcassidy1@gmail.com>
2026-02-01 17:00:03 +0000

    letter

---
+++ Assets/Scripts/LetterBehaviour.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class LetterBehaviour : MonoBehaviour
+{
+
+    bool focussed = false;
+
+    public GameObject letter;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !focussed)
+        {
+            focussed = true;
+            CameraController.current.cameraLocked = true;
+            letter.SetActive(true);
+        }
+    }
+
+    public void Unfocus() {
+        focussed = false;
+        CameraController.current.cameraLocked = false;
+        letter.SetActive(false);
+    }
+}
